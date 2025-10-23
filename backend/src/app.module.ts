c3e1f0cc24d7508c7e06808c480d7d597440b297
Hashan Perera <hashperera.v@gmail.com>
2025-10-24 00:38:42 +0530

    Dockerize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './modules/todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Database connection using env vars
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: parseInt(configService.get<string>('DB_PORT', '5432')),
+        username: configService.get<string>('DB_USER', 'postgres'),
+        password: configService.get<string>('DB_PASS', '0000'),
+        database: configService.get<string>('DB_NAME', 'todo'),
+        autoLoadEntities: true,
+        synchronize: true, // set to false in production
+      }),
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
